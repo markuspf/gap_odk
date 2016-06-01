ab5b77ffafad34c34afc185de722c8a2b9e65dd3
m-iancu <mihnea.iancu88@gmail.com>
2016-06-01 12:42:19 +0200

    added names comparison from doc and sys

---
+++ gap/compare.py
@@ -0,0 +1,38 @@
+#!env python
+sn = open('namesSys.txt', 'r')
+dn = open('namesDoc.txt', 'r')
+sysNames = set(sn.readlines())
+docNames = set(dn.readlines())
+
+shared = []
+docOnly = []
+sysOnly = []
+
+def mkstring(list):
+	return "".join(list)
+
+for name in sysNames:
+	if name in docNames:
+		shared.append(name)
+	else:
+		sysOnly.append(name)
+
+for name in docNames:
+	if name not in sysNames:
+		docOnly.append(name)		
+
+sn = open('sharedNames.txt', 'w')
+sn.write(mkstring(shared))
+sn.close
+
+don = open('docOnlyNames.txt', 'w')
+don.write(mkstring(docOnly))
+don.close
+
+son = open('sysOnlyNames.txt', 'w')
+son.write(mkstring(sysOnly))
+son.close
+
+print 'Shared:   ', len(shared)
+print 'Doc Only: ', len(docOnly)
+print 'Sys Only: ', len(sysOnly)
---
+++ gap/getNames.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Utility command to get all symbol names from the JSON
+if [[ $# -eq 0 ]] ; then
+    echo "Requires json filename as argument"
+    exit 1
+fi
+cat $1 | jq '[.[] | .name]' | sed 's/Setter(\(.*\))/Set\1/' | sed 's/Tester(\(.*\))/Is\1/' | sed 's/(.*)//' | sed 's/["|,| ]//g'
